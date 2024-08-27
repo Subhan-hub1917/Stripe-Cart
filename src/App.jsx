7c7d5a9eb43e9d7927d9241cb03e9162ec26e20b
Subhan Qamar <subiq1917@gmail.com>
2024-08-27 12:33:01 +0500

    API endpoint and webhook added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Cart from './Components/Cart'
+import Cart  from './Components/Cart'
 import Success from './Pages/Success'
 import Cancel from './Pages/Cancel'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
@@ -7,15 +7,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <main>
     <Router>
+      <main>
       <Routes>
-        <Route path='/' element={<Cart/>}/>
-        <Route path='/success' element={<Success/>}/>
-        <Route path='/cancel' element={<Cancel/>}/>
+        <Route exact path='/' element={<Cart/>}/>
+        <Route exact path='/success' element={<Success/>}/>
+        <Route exact path='/cancel' element={<Cancel/>}/>
       </Routes>
-    </Router>
-      </main>
+    </main>
+  </Router>
   )
 }
 
